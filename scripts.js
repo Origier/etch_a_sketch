a9cca37a3340e5711607ec7c844a4d984b999d9d
Origier <sergeantbeast456@gmail.com>
2018-11-09 15:46:51 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let sketchContainer = document.querySelector('#sketch-container');
+let sketchRow = document.createElement('div');
+let sketchPixel = document.createElement('div');
+let numberOfPixels = 16;
+
+sketchRow.classList.add('row');
+sketchPixel.classList.add('pixel');
+
+//start by creating a 16x16 grid || listen for the users input on how many pixels they want
+
+
+
+//create the number of rows based on the user input
+
+//append pixels to each row for the number the user input
+
+//start a listener for all the pixels listening for a mouse hover
+
+//when mousehover == true toggle the pixel to the class pixel darkened, if it is already this class then ignore it
