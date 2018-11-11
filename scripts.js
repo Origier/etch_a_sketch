9222933653ae42bf495a0d856aa97666624bfbc0
Origier <sergeantbeast456@gmail.com>
2018-11-11 04:03:03 -0500

    operational sketch pad

--- scripts.js
+++ scripts.js
@@ -1,34 +1,28 @@
 const pixelDemand = document.querySelector('#pixel-demand');
 const pixelateBtn = document.querySelector('#pixelate');
-let sketchContainer = document.querySelector('#sketch-container');
-let sketchRows = [];  
+let sketchContainer = document.querySelector('#sketch-container'); 
 let sketchPixels = [];
+let sketchRows = [];
 let numberOfPixels = 16;
+let pixelHeight;
 let initialRun = false;
 
-pixelateBtn.addEventListener('click', pixelateContainer);
+pixelateBtn.addEventListener('click', pixelateContainer)
 
-initialize();
-
-function initialize() {
-  if (initialRun) {
-    return;
-  } else {
-    initialRun = true;
-    createGrid(numberOfPixels);
-  }
-}
+createGrid(numberOfPixels);
 
 function pixelateContainer() {
   removeGrid(numberOfPixels);
   numberOfPixels = pixelDemand.value;
   numberOfPixels = parseInt(numberOfPixels);
-  createGrid(numberOfPixels);
-  return;
+  if(isNaN(numberOfPixels)){
+    alert('Try entering an integer.');
+  } else {
+    createGrid(numberOfPixels);
+  }
 }
 
 function createGrid(pixels) {
-  alert(numberOfPixels);
   for (let i = 0; i < pixels; i++) {
     createRow(i);
     for (let j = 0; j < pixels; j++) {
@@ -37,14 +31,42 @@ function createGrid(pixels) {
     }
     sketchContainer.appendChild(sketchRows[i]);
   }
+  if (pixelHeight % 1 == 0){
+    sketchContainer.setAttribute('style', `height: 900px; width: 900px;`);
+  } else {
+    sketchContainer.setAttribute('style', `height: ${(pixelHeight * numberOfPixels).toFixed(2) -3}px; width: ${(pixelHeight * numberOfPixels).toFixed(2) -3}px;`);
+  }
+  createHoverListener();
+}
+
+function createListener() {
+  sketchContainer.addEventListener('mousedown', createHoverListener);
+}
+
+function createHoverListener() {
+  const pixels = document.querySelectorAll('.pixel');
+  pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', (e) => {
+      changeColor(e);
+    });
+  });
+}
+
+
+function removeHoverListener() {
+  pixels.forEach((pixel) => {
+    pixel.removeEventListener('mouseover', (e) => {
+      changeColor(e);
+    });
+  });
 }
 
 function removeGrid(pixels) {
   for (let i = 0; i < pixels; i++) {
     sketchContainer.removeChild(sketchRows[i]);
   }
-  sketchRows = [];
   sketchPixels = [];
+  sketchRows = [];
 }
 
 function createRow(i) {
@@ -53,14 +75,12 @@ function createRow(i) {
 }
 
 function createPixel(j, pixels) {
-  let height = 900/pixels;
+  pixelHeight = (900/pixels).toFixed(2);
   sketchPixels[j] = document.createElement('div');
   sketchPixels[j].classList.add('pixel');
-  sketchPixels[j].setAttribute('style', `height: ${height}px; width: ${height}px;`);
+  sketchPixels[j].setAttribute('style', `height: ${pixelHeight}px; width: ${pixelHeight}px;`);
 }
 
-//append pixels to each row for the number the user input
-
-//start a listener for all the pixels listening for a mouse hover
-
-//when mousehover == true toggle the pixel to the class pixel darkened, if it is already this class then ignore it
+function changeColor(e) {
+  e.target.classList.add('black');
+}
